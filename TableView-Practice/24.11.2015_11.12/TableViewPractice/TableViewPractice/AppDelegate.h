a40365af386905623015b799a55d5471757a2a39
Tony <ntutonycao@gmail.com>
2015-11-24 11:13:14 +0800

    Modified the folder name

---
+++ TableView-Practice/24.11.2015_11.12/TableViewPractice/TableViewPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewPractice
+//
+//  Created by Cao Lei on 24/11/15.
+//  Copyright (c) 2015 Cao Lei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableView-Practice/24.11.2015_11.12/TableViewPractice/TableViewPractice/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TableViewPractice
+//
+//  Created by Cao Lei on 24/11/15.
+//  Copyright (c) 2015 Cao Lei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
