91cc59210c8649cfb7ca56dd25f96845b7b34ce7
Tony <ntutonycao@gmail.com>
2016-02-03 14:00:37 +0800

    Added the files.
    Accelerometer and Gyro Scope

---
+++ Accelerometer-Simple/GyrosAndAccelerometers/ViewController.h
@@ -0,0 +1,43 @@
+//
+//  ViewController.h
+//
+//
+
+
+#import <UIKit/UIKit.h>
+#import <CoreMotion/CoreMotion.h>
+
+
+double currentMaxAccelX;
+double currentMaxAccelY;
+double currentMaxAccelZ;
+double currentMaxRotX;
+double currentMaxRotY;
+double currentMaxRotZ;
+
+@interface ViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *accX;
+@property (strong, nonatomic) IBOutlet UILabel *accY;
+@property (strong, nonatomic) IBOutlet UILabel *accZ;
+
+@property (strong, nonatomic) IBOutlet UILabel *maxAccX;
+@property (strong, nonatomic) IBOutlet UILabel *maxAccY;
+@property (strong, nonatomic) IBOutlet UILabel *maxAccZ;
+
+@property (strong, nonatomic) IBOutlet UILabel *rotX;
+@property (strong, nonatomic) IBOutlet UILabel *rotY;
+@property (strong, nonatomic) IBOutlet UILabel *rotZ;
+
+@property (strong, nonatomic) IBOutlet UILabel *maxRotX;
+@property (strong, nonatomic) IBOutlet UILabel *maxRotY;
+@property (strong, nonatomic) IBOutlet UILabel *maxRotZ;
+
+
+
+
+- (IBAction)resetMaxValues:(id)sender;
+
+@property (strong, nonatomic) CMMotionManager *motionManager;
+
+@end
