7c36c21e02b1eb5cb5db557d7a4bda26a2c80224
Tony <ntutonycao@gmail.com>
2016-03-25 17:27:39 +0800

    Added files.

---
+++ FaceDetectionSelf/CreateFaceDetectionTest/CreateFaceDetectionTest/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  CreateFaceDetectionTest
+//
+//  Created by Lei Cao on 24/3/16.
+//  Copyright © 2016 Lei Cao (Tony). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <opencv2/opencv.hpp>
+#import <opencv2/highgui/cap_ios.h>
+
+@interface ViewController : UIViewController<CvVideoCameraDelegate>
+
+@end
+
