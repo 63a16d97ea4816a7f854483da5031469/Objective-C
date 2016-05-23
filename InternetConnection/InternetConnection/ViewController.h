0fe1e40434763ce72359894e3eb020211a7e7149
Tony <ntutonycao@gmail.com>
2016-05-23 14:57:56 +0800

    Added the files.

---
+++ InternetConnection/InternetConnection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InternetConnection
+//
+//  Created by Lei Cao on 23/5/16.
+//  Copyright © 2016 Lei Cao (Tony). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
